a5aad3ddba80ade3b1944f68bbbf8773be28133a
Ahmet E. Dag <adag21044@gmail.com>
2024-08-08 16:46:36 +0300

    Initialize

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public Text scoreText;
+    public GameObject PlayButton;
+    private int score;
+    public GameObject gameOver;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        PlayButton.SetActive(false);
+        gameOver.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+    
+    public void GameOver()
+    {
+        gameOver.SetActive(true);  
+        PlayButton.SetActive(true);
+        Pause();
+    }
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
